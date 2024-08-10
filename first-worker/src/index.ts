54c280cd981220d159017b12f1d51f75377fb5f0
harshilp25 <harshildpatel255@gmail.com>
2024-08-10 07:15:10 +0530

    "final changes"

--- first-worker/src/index.ts
+++ first-worker/src/index.ts
@@ -14,14 +14,22 @@
 export default {
 	// default export
 	async fetch(request, env, ctx): Promise<Response> {
+		const body: any = await request.json(); // gives the body after converting to json :) but if the body is in the form of text then run .text( method to parse it in the txt ;)
+
 		// will be called whenever request goes out
 		// as we can see the syntax is slightly complicated so we are gonna using the external libaray or framework to make routes :)
 
 		// request.method give you the type of request came to here
+
 		if (request.method == 'POST') {
 			if (request.url.includes('/users/signin')) {
 				// like this we can make a route in this :) but hard to debug the code as well :)
-				return new Response('inside the sigin route ');
+
+				console.log(body);
+				console.log(request.headers.has('auth')); // either returns null or the associated value of the certain header :)
+				return Response.json({
+					success: 'inside the signin route',
+				});
 			}
 			return Response.json({
 				success: 'its a post reqeust',
